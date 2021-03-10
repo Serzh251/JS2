6bcf21eb22993b96849c9e2f5af04eebec3de0f3
serzh251 <serzh251@mail.ru>
2021-03-10 21:00:50 +0300

    Lesson8_source

---
+++ Lesson8/lesson8_09.03.21/webpack_app/src/public/index.js
@@ -0,0 +1,8 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import {App} from './js/main';
+import './css/normalize.css';
+import './css/style.css';
+import './sass/main.scss'
+
+Vue.createApp(App).mount('#app');
